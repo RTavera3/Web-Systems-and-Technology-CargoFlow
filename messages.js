13e412e8f6c3c3ffec0b1e8c04265811b4b30688
MitziReese04 <lr.mrarrogante@mmdc.mcl.edu.ph>
2026-09-16 17:01:19 +0800

    Update JS for messages - defining variables

---
+++ messages.js
@@ -0,0 +1,4 @@
+// Select the chat elements we need to use.
+const messageInput = document.querySelector('.message-input');
+const sendButton = document.querySelector('.send-btn');
+const messagesContainer = document.querySelector('.messages-container');
